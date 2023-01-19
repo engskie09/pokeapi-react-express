a600a6651d09cbab48be1d0665cc610d5af9ff02
Rafael Masallo <rafaelmasallo@gmail.com>
2023-01-20 00:35:32 +0800

    🛠️ setup database connection and 🌱 dev environment

--- server/server.js
+++ server/server.js
@@ -1,6 +1,22 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+mongoose.connect(process.env.DATABASE_URL);
+const database = mongoose.connection
+
+database.on('error', (error) => {
+  console.log(error)
+})
+
+database.once('connected', () => {
+  console.log('Database Connected');
+})
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
