37656c29989924072957d72f58b5b23c7d05d1a7
Rafael Masallo <rafaelmasallo@gmail.com>
2023-01-21 05:53:43 +0800

    🎣 added pokemons hook with API implementation and 🖼️ implemented it to UI component

--- client/src/components/pokemons.tsx
+++ client/src/components/pokemons.tsx
@@ -4,28 +4,33 @@ import { ThemeStyleType } from '../utilities/style';
 import { useLogin } from '../hooks/login';
 
 import pokemonLogo from '../assets/pokemon-logo.png';
+import { usePokemons } from '../hooks/pokemons';
 
 const style = {
     container: { marginTop: 5 } as ThemeStyleType,
 };
 
-export const Pokemons = () => (
-    <Container maxWidth="md" sx={style.container}>
-        <Box sx={{ flexGrow: 1 }}>
-            <Grid container spacing={2}>
-                <Grid item md={3}>
-                    Pokemon
+export const Pokemons = () => {
+    usePokemons();
+
+    return (
+        <Container maxWidth="md" sx={style.container}>
+            <Box sx={{ flexGrow: 1 }}>
+                <Grid container spacing={2}>
+                    <Grid item md={3}>
+                        Pokemon
+                    </Grid>
+                    <Grid item md={3}>
+                        Pokemon
+                    </Grid>
+                    <Grid item md={3}>
+                        Pokemon
+                    </Grid>
+                    <Grid item md={3}>
+                        Pokemon
+                    </Grid>
                 </Grid>
-                <Grid item md={3}>
-                    Pokemon
-                </Grid>
-                <Grid item md={3}>
-                    Pokemon
-                </Grid>
-                <Grid item md={3}>
-                    Pokemon
-                </Grid>
-            </Grid>
-        </Box>
-    </Container>
-);
+            </Box>
+        </Container>
+    );
+};
--- client/src/hooks/pokemons.tsx
+++ client/src/hooks/pokemons.tsx
@@ -1,3 +1,13 @@
+import { pokemonAPI } from '../utilities/store/slice/api';
+
+import { useAppSelector, useAppDispatch, pokemonComponent } from '../utilities/store';
+
 export const usePokemons = () => {
-    
-}
+    const dispatch = useAppDispatch();
+
+    pokemonAPI.usePokemonsQuery();
+
+    const pokemons: any[] = useAppSelector((state) => state.pokemonComponent.pokemons);
+
+    console.log(pokemons)
+};
