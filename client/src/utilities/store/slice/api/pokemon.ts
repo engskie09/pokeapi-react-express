906112db244e16bd7072fc617e852e1b26f7c546
Rafael Masallo <rafaelmasallo@gmail.com>
2023-01-21 05:52:32 +0800

    🔒 setup Authorization header on pokemonAPI, and ➕ added pokemonComponent

--- client/src/utilities/store/slice/api/pokemon.ts
+++ client/src/utilities/store/slice/api/pokemon.ts
@@ -1,11 +1,22 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { snakeKeys } from 'js-convert-case';
 
+import type { RootState } from '../../store';
+
 export const pokemonAPI = createApi({
     reducerPath: 'pokemonAPI',
     tagTypes: ['pokemons'],
     baseQuery: fetchBaseQuery({
         baseUrl: `${import.meta.env.VITE_API_URL}`,
+        prepareHeaders: (headers, { getState }) => {
+            const { token } = (getState() as RootState).sessionComponent;
+
+            if (token) {
+                headers.set('Authorization', `Bearer ${token}`);
+            }
+
+            return headers;
+        },
     }),
     endpoints(builder) {
         return {
