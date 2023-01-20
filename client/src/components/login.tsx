2bb8029d4637425f4d117aa00d4953c28728ee1a
Rafael Masallo <rafaelmasallo@gmail.com>
2023-01-21 01:19:41 +0800

    🚧 WIP on UI Login and ➕ added initial files and utilities

--- client/src/components/login.tsx
+++ client/src/components/login.tsx
@@ -1,3 +1,85 @@
-export const Login = () => {
 
-}
+import { CSSProperties } from 'react';
+import { Link } from 'react-router-dom';
+import { FormProvider, Controller } from 'react-hook-form';
+
+import { 
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    Grid,
+    Paper,
+    TextField,
+    Typography,
+    Box,
+    ButtonGroup,
+} from '@mui/material';
+
+import { ThemeStyleType } from '../utilities/style';
+
+const style = {
+    container: { maxWidth: 400 } as ThemeStyleType,
+    icon: { fontSize: '70px', margin: 'auto', width: '100%', marginTop: -4 } as ThemeStyleType,
+    title: { textAlign: 'center' } as ThemeStyleType,
+    action: {
+        container: { marginBottom: '10px' } as ThemeStyleType,
+        button: { marginBottom: 2 } as ThemeStyleType,
+        link: { textDecoration: 'none', color: 'inherit' } as CSSProperties,
+    },
+};
+
+export const Login = () => (
+    <Paper component="form" noValidate autoComplete="off" sx={style.container}>
+        <Card>
+            <CardContent>
+                <Typography variant="h4" component="p" sx={style.title}>
+                    Login
+                </Typography>
+                <Grid container>
+                    <Grid item sm={12} marginBottom={1} width="100%">
+                        <Controller
+                            name="username"
+                            render={({ field: { onChange, value } }) => (
+                                <TextField
+                                    label="Username"
+                                    value={value || ''}
+                                    variant="standard"
+                                    fullWidth
+                                    onChange={onChange}
+                                />
+                            )}
+                        />
+                    </Grid>
+                    <Grid item sm={12} width="100%">
+                        <Controller
+                            name="password"
+                            render={({ field: { onChange, value } }) => (
+                                <TextField
+                                    label="Password"
+                                    type="password"
+                                    value={value || ''}
+                                    variant="standard"
+                                    fullWidth
+                                    onChange={onChange}
+                                />
+                            )}
+                        />
+                    </Grid>
+                </Grid>
+            </CardContent>
+            <CardActions sx={style.action.container}>
+                <ButtonGroup orientation="vertical" fullWidth>
+                    <Button variant="contained" type="submit" sx={style.action.button}>
+                        Login
+                    </Button>
+                    <Button variant="text">
+                        <Link to="/sign-up" style={style.action.link}>
+                            Not registered yet? signup now.
+                        </Link>
+                    </Button>
+                </ButtonGroup>
+            </CardActions>
+        </Card>
+    </Paper>
+);
---
+++ client/src/utilities/style.ts
@@ -0,0 +1,3 @@
+import { SxProps } from '@mui/system';
+
+export type ThemeStyleType = SxProps<any>;
