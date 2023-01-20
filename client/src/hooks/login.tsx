00f9f2654ad426a0c50253543028e171616157cf
Rafael Masallo <rafaelmasallo@gmail.com>
2023-01-21 02:39:48 +0800

    🏗️ implemented trainer login api on login component and ✏️ updated .env.example

--- client/src/hooks/login.tsx
+++ client/src/hooks/login.tsx
@@ -2,15 +2,25 @@ import { useState, FormEvent } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 
+import { trainerAPI } from '../utilities/store';
+
 export const useLogin = () => {
     const form = useForm<any>({
         defaultValues: {},
     });
 
+    const [login] = trainerAPI.useLoginMutation();
+
     const handleOnSubmit = async (event: FormEvent) => {
         event.preventDefault();
 
-        alert('login');
+        login(form.getValues()).then((response) => {
+            if ('data' in response) {
+                alert('success');
+            } else if ('error' in response && 'status' in response.error) {
+                alert('try again');
+            }
+        });
     };
 
     return { form, handleOnSubmit };
