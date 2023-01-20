72354dbe78bb604079ed4477e9b11252ed01ec27
Rafael Masallo <rafaelmasallo@gmail.com>
2023-01-21 02:00:24 +0800

    🖼️ setup container to center the component

--- client/src/components/login.tsx
+++ client/src/components/login.tsx
@@ -2,13 +2,25 @@ import { CSSProperties } from 'react';
 import { Link } from 'react-router-dom';
 import { FormProvider, Controller } from 'react-hook-form';
 
-import { Button, Card, CardActions, CardContent, Grid, Paper, TextField, Typography, ButtonGroup } from '@mui/material';
+import {
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    Container,
+    Grid,
+    Paper,
+    TextField,
+    Typography,
+    ButtonGroup,
+} from '@mui/material';
 
 import { ThemeStyleType } from '../utilities/style';
 import { useLogin } from '../hooks/login';
 
 const style = {
-    container: { maxWidth: 400 } as ThemeStyleType,
+    container: { marginTop: 5 } as ThemeStyleType,
+    form: { maxWidth: 400 } as ThemeStyleType,
     icon: { fontSize: '70px', margin: 'auto', width: '100%', marginTop: -4 } as ThemeStyleType,
     title: { textAlign: 'center' } as ThemeStyleType,
     action: {
@@ -22,59 +34,61 @@ export const Login = () => {
     const { form, handleOnSubmit } = useLogin();
 
     return (
-        <FormProvider {...form}>
-            <Paper component="form" onSubmit={handleOnSubmit} noValidate autoComplete="off" sx={style.container}>
-                <Card>
-                    <CardContent>
-                        <Typography variant="h4" component="p" sx={style.title}>
-                            Login
-                        </Typography>
-                        <Grid container>
-                            <Grid item sm={12} marginBottom={1} width="100%">
-                                <Controller
-                                    name="username"
-                                    render={({ field: { onChange, value } }) => (
-                                        <TextField
-                                            label="Username"
-                                            value={value || ''}
-                                            variant="standard"
-                                            fullWidth
-                                            onChange={onChange}
-                                        />
-                                    )}
-                                />
-                            </Grid>
-                            <Grid item sm={12} width="100%">
-                                <Controller
-                                    name="password"
-                                    render={({ field: { onChange, value } }) => (
-                                        <TextField
-                                            label="Password"
-                                            type="password"
-                                            value={value || ''}
-                                            variant="standard"
-                                            fullWidth
-                                            onChange={onChange}
-                                        />
-                                    )}
-                                />
-                            </Grid>
-                        </Grid>
-                    </CardContent>
-                    <CardActions sx={style.action.container}>
-                        <ButtonGroup orientation="vertical" fullWidth>
-                            <Button variant="contained" type="submit" sx={style.action.button}>
+        <Container maxWidth="xs" sx={style.container}>
+            <FormProvider {...form}>
+                <Paper component="form" onSubmit={handleOnSubmit} noValidate autoComplete="off" sx={style.form}>
+                    <Card>
+                        <CardContent>
+                            <Typography variant="h4" component="p" sx={style.title}>
                                 Login
-                            </Button>
-                            <Button variant="text">
-                                <Link to="/sign-up" style={style.action.link}>
-                                    Not registered yet? signup now.
-                                </Link>
-                            </Button>
-                        </ButtonGroup>
-                    </CardActions>
-                </Card>
-            </Paper>
-        </FormProvider>
+                            </Typography>
+                            <Grid container>
+                                <Grid item sm={12} marginBottom={1} width="100%">
+                                    <Controller
+                                        name="username"
+                                        render={({ field: { onChange, value } }) => (
+                                            <TextField
+                                                label="Username"
+                                                value={value || ''}
+                                                variant="standard"
+                                                fullWidth
+                                                onChange={onChange}
+                                            />
+                                        )}
+                                    />
+                                </Grid>
+                                <Grid item sm={12} width="100%">
+                                    <Controller
+                                        name="password"
+                                        render={({ field: { onChange, value } }) => (
+                                            <TextField
+                                                label="Password"
+                                                type="password"
+                                                value={value || ''}
+                                                variant="standard"
+                                                fullWidth
+                                                onChange={onChange}
+                                            />
+                                        )}
+                                    />
+                                </Grid>
+                            </Grid>
+                        </CardContent>
+                        <CardActions sx={style.action.container}>
+                            <ButtonGroup orientation="vertical" fullWidth>
+                                <Button variant="contained" type="submit" sx={style.action.button}>
+                                    Login
+                                </Button>
+                                <Button variant="text">
+                                    <Link to="/sign-up" style={style.action.link}>
+                                        Not registered yet? signup now.
+                                    </Link>
+                                </Button>
+                            </ButtonGroup>
+                        </CardActions>
+                    </Card>
+                </Paper>
+            </FormProvider>
+        </Container>
     );
 };
